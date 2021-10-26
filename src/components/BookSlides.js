ced455cf5309aa9ddccfc2a0b2517835c8573f26
Maximiliano <calderonmaxi@outlook.com>
2021-10-25 23:55:42 -0300

    books api using mockachino

--- src/components/BookSlides.js
+++ src/components/BookSlides.js
@@ -5,13 +5,14 @@ import useAxiosBooks from "../hooks/useAxiosBooks";
 import BookItem from "./BookItem";
 
 const BookSlides = ({ onAdd }) => {
-  const url = "https://www.mockachino.com/spaces/96da619c-bdcb-48";
+  const url = "https://www.mockachino.com/96da619c-bdcb-48/books";
 
   const books = useAxiosBooks(url);
-
+  
+console.log(books)
   return (
     <BookSlidesWork controles={true}>
-      {books.map((book) => (
+      {(books.length === 0 ? books : books.books).map((book) => (
         <BookItem onAdd={onAdd} key={book.id} {...book} />
       ))}
     </BookSlidesWork>
